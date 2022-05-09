5c92c8e58c3ae95a7246c14d72feb2fd94685313
VijayaprakashVp <prakashvijay905@gmail.com>
2022-05-09 18:09:38 +0530

    Update

--- MAC_10_Day_Project/Frontend/Own_E-Commerce/src/App.jsx
+++ MAC_10_Day_Project/Frontend/Own_E-Commerce/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
+// import ShopIcon from "@mui/icons-material/Shop";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{/* <ShopIcon /> */}</div>;
 }
 
 export default App;

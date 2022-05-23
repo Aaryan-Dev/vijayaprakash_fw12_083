77c88e69317b03dd1c0f6fbb61c74d9294d2273d
VijayaprakashVp <prakashvijay905@gmail.com>
2022-05-23 12:38:04 +0530

    sign in page and css

--- MAC_10_Day_Project/Frontend/Own_E-Commerce/src/Components/SignIn/SignIn.jsx
+++ MAC_10_Day_Project/Frontend/Own_E-Commerce/src/Components/SignIn/SignIn.jsx
@@ -33,37 +33,59 @@ export const SignIn = () => {
       </div>
       <div>
         return (
-        <div className="signup-cont">
-          <div>
-            <h3>Welcome to Shopify</h3>
-            <br />
-            <br />
-            <p>Enter Mail Id</p>
-            <input
-              type="email"
-              name="email"
-              value={userdetails.email}
-              onChange={(e) => handleSave(e)}
-            />
-            <br />
-            <br />
-            <p>Enter Password</p>
-            <input
-              type="text"
-              name="password"
-              value={userdetails.password}
-              onChange={(e) => handleSave(e)}
-            />
-            <br />
-            <br />
-            <br />
-            <Link to={"/"}>
-              <button onClick={handleSubmit}>Sign in</button>
-            </Link>
+        <div id="sign_in">
+          <div id="sign_in_div">
+            <div>
+              <h3>Welcome to Shopify</h3>
+              <h2>Sign In</h2>
+            </div>
+            <div id="sign_in_form">
+              <form action="">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter Email"
+                  style={{ borderRadius: "20px", paddingLeft: "5%" }}
+                  value={userdetails.email}
+                  onChange={(e) => handleSave(e)}
+                />
+                <br />
+                <br />
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="Enter Password"
+                  value={userdetails.password}
+                  style={{ borderRadius: "20px", paddingLeft: "5%" }}
+                  onChange={(e) => handleSave(e)}
+                />
+                <br />
+              </form>
+              <a href="#" className="forgot_Password">
+                Forgot your password?
+              </a>
+              <div>
+                <br />
+                <Link to={"/"}>
+                  <button
+                    onClick={handleSubmit}
+                    className="button-33"
+                    style={{ backgroundColor: "#5fafc4", color: "black" }}
+                  >
+                    Sign in
+                  </button>
+                </Link>
+                <p className="sign_up_link">
+                  New To{" "}
+                  <span style={{ color: "red", fontWeight: 600 }}>
+                    Shopp.my?{" "}
+                  </span>
+                  <a href="./Sign_Up.html">Create Your Account</a>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
         <Footer />
       </div>
     </div>

3d8504af403352a75cf7ac1d95f79ed903deec88
VijayaprakashVp <prakashvijay905@gmail.com>
2022-05-24 13:37:09 +0530

    Order confirmation page created and works fine

--- MAC_10_Day_Project/Frontend/Own_E-Commerce/src/Components/OrderConfirmation/OrderConfirmation.jsx
+++ MAC_10_Day_Project/Frontend/Own_E-Commerce/src/Components/OrderConfirmation/OrderConfirmation.jsx
@@ -6,7 +6,9 @@ import { Footer } from "../Footer/Footer";
 
 export const OrderConfirmation = () => {
   const [orderStatus, setOrderStatus] = useState(false);
-  console.log("orderStatus:", orderStatus);
+  const [transId, setTransId] = useState(Math.random().toString(36).slice(2));
+  console.log("transId:", transId);
+  // console.log("orderStatus:", orderStatus);
 
   useEffect(() => {
     setTimeout(() => {
@@ -19,15 +21,37 @@ export const OrderConfirmation = () => {
       <div>
         <NavBar />
       </div>
-      <div
-        style={{ marginTop: "15vh", border: "1px solid red", height: "50vh" }}
-      >
+      <div style={{ marginTop: "15vh", height: "65vh" }}>
         {orderStatus ? (
-          <p>1234</p>
-        ) : (
           <div>
-            <BeatLoader />
-            <BeatLoader />
+            <img
+              src="https://cdn.dribbble.com/users/571236/screenshots/2888472/happystate.gif"
+              alt=""
+              style={{ width: "25%" }}
+            />
+            <h4>
+              <span style={{ color: "red", fontWeight: 600 }}>Yay!</span> Order
+              Placed succesfully
+            </h4>
+            <p>
+              Transaction Id : <span style={{ color: "blue" }}>{transId}</span>
+            </p>
+            <p>
+              Products will deliver in{" "}
+              <span style={{ color: "red", fontWeight: 600 }}>3 Days!</span>
+            </p>
+          </div>
+        ) : (
+          <div
+            style={{
+              marginTop: "13%",
+            }}
+          >
+            Payment Processing
+            <p>
+              <BeatLoader />
+              <BeatLoader />
+            </p>
           </div>
         )}
       </div>

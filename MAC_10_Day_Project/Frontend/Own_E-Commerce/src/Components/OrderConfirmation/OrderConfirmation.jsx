e6c001b19198e745fc8dd2a737fba830a2aae686
VijayaprakashVp <prakashvijay905@gmail.com>
2022-05-24 13:06:50 +0530

    Update OrderConfirmation.jsx

--- MAC_10_Day_Project/Frontend/Own_E-Commerce/src/Components/OrderConfirmation/OrderConfirmation.jsx
+++ MAC_10_Day_Project/Frontend/Own_E-Commerce/src/Components/OrderConfirmation/OrderConfirmation.jsx
@@ -1,34 +1,45 @@
-import React from "react";
-
-export const OrderConfirmation = () => {
-  return <div>OrderConfirmation</div>;
-};
-
-// import React, { useEffect, useState } from "react";
-// import "./OrderConfirmation.css";
-// import { BarLoader } from "react-spinners";
-// import { NavBar } from "../NavBar/NavBar";
-// import { Footer } from "../Footer/Footer";
+// import React from "react";
 
 // export const OrderConfirmation = () => {
-//   const [orderStatus, setOrderStatus] = useState(false);
-//   console.log("orderStatus:", orderStatus);
-
-//   useEffect(() => {
-//     setTimeout(() => {
-//       setOrderStatus(true);
-//     }, 5000);
-//   }, []);
-
-//   return (
-//     <div>
-//       <div>
-//         <NavBar />
-//       </div>
-//       <div>{orderStatus ? <p>1234</p> : <BarLoader />}</div>
-//       <div>
-//         <Footer />
-//       </div>
-//     </div>
-//   );
+//   return <div>OrderConfirmation</div>;
 // };
+
+import React, { useEffect, useState } from "react";
+import "./OrderConfirmation.css";
+import { BarLoader, BounceLoader, BeatLoader } from "react-spinners";
+import { NavBar } from "../NavBar/NavBar";
+import { Footer } from "../Footer/Footer";
+
+export const OrderConfirmation = () => {
+  const [orderStatus, setOrderStatus] = useState(false);
+  console.log("orderStatus:", orderStatus);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setOrderStatus(true);
+    }, 5000);
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div
+        style={{ marginTop: "15vh", border: "1px solid red", height: "50vh" }}
+      >
+        {orderStatus ? (
+          <p>1234</p>
+        ) : (
+          <div>
+            <BeatLoader />
+            <BeatLoader />
+          </div>
+        )}
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
